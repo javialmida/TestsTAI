aacf6f6b8a320716ffcedec626ea09b0480d1ae2
javialmida <javialmida@gmail.com>
2026-05-03 14:46:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -632,11 +632,14 @@ const app = {
 
     finalizar: async () => {
         app.stopTimer(); 
+        state.seconds = 0;
+        document.getElementById('timer').innerText = app.formatTime(0);
+        document.getElementById('timer').classList.add('hidden');
         await app.borrarProgreso(); 
         
         app.switchView('view-results');
         //NUEVO----------------------------------------------------------
-        app.setBtnSalir('volver');
+        app.setBtnSalir('salir');
         //----------------------------------------------------------------
         document.getElementById('counter').classList.add('hidden');
 
